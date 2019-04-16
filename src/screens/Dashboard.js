b4dc5797cac93240179a76d758d3f32b8698a44a
Mezbaul Haque <mezba109@gmail.com>
2019-04-16 13:13:13 +0600

    fix bug in Dashboard

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import NotFoundScreen from './NotFound';
-import TopNavigationMenu from '../components/Menu/TopNavigation';
+// import TopNavigationMenu from '../components/Menu/TopNavigation';
 
 
 class Dashboard extends Component {
